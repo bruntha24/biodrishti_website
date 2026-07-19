6000d376a899cfc1b5318c878e5afde0085647ed
M.Bruntha <m.bruntha24@gmail.com>
2026-07-19 22:29:30 +0530

    updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
 import { createRoot } from "react-dom/client";
+
+import "@fontsource/dm-sans/400.css";
+import "@fontsource/dm-sans/500.css";
+import "@fontsource/dm-sans/600.css";
+
+import "@fontsource/dm-serif-display/400.css";
+
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <App />
+);
